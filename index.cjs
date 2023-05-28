1593cda6475ebafe4f8b783fadaac3ab200070f1
Jefte Sales <jefte.green@gmail.com>
2023-05-28 14:54:27 -0300

    update files

---
+++ index.cjs
@@ -0,0 +1,21 @@
+const express = require('express');
+const { readFileSync } = require('fs');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const postsData = require('./src/Posts/posts.json');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  return res.json('Hello world');
+});
+
+app.get('/posts', (req, res) => {
+  return res.json(posts);
+});
+
+app.listen(port, () => {
+  console.log('Servidor está funcionando');
+});
