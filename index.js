dfab6a04959bcc71b446e8d0a0bb7e8c15ce7819
Jefte Sales <jefte.green@gmail.com>
2023-05-28 14:21:02 -0300

    update files

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const express = require('express')
+const express = require('express');
 const app  = express()
+app.use(express.json())
 const port = process.env.PORT || 3000
 
 const posts = require('./src/Posts/posts.json')
-
+app.get("/", (req,res) => {
+    return res.json("Hello world")
+})
 app.get('/posts', (req, res) => {
     return res.json(posts)
 })
+
 
 app.listen(port, () => {
     console.log('servidor esta funcionando')
