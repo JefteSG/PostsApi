b325a9ce62d2e1c0ed8584c0da0820cad590716e
Jefte Sales <jefte.green@gmail.com>
2023-05-28 15:50:09 -0300

    update files

--- index.cjs
+++ index.cjs
@@ -1,12 +1,14 @@
 const express = require('express');
 const { readFileSync } = require('fs');
+const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const postsData = require('./src/Posts/posts.json');
 
 app.use(express.json());
+app.use(cors())
 
 app.get('/', (req, res) => {
   return res.json('Hello world');
