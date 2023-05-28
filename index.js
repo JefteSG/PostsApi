f43e04c6189508980b7d54631a075b1e130093fe
Jefte Sales <jefte.green@gmail.com>
2023-05-28 14:30:56 -0300

    update files

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express');
+import express from 'express';
 const app  = express()
 app.use(express.json())
 const port = process.env.PORT || 3000
+import { readFileSync } from 'fs';
 
-const posts = require('./src/Posts/posts.json')
+const postsData = readFileSync('./src/Posts/posts.json', 'utf8');
+const posts = JSON.parse(postsData);
+
 app.get("/", (req,res) => {
     return res.json("Hello world")
 })
